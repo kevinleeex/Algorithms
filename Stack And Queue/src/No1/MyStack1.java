8f0498a60449167f422fa2af79beed160ab09b7b
Kevin <kevinleeex@foxmail.com>
2016-08-15 23:44:16 +0800

    no message

---
+++ Stack And Queue/src/No1/MyStack1.java
@@ -0,0 +1,11 @@
+package No1;
+
+/**
+ * Description: <br/>
+ * Author: KevinLee <br/>
+ * Version: 1.0 <br/>
+ * Create Date Time: 2016-08-15 23:35. <br/>
+ * Update Date Time: <br/>
+ */
+public class MyStack1 {
+}
